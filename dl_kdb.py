2f15ae2a592c321defa4af7ee099d0db77621383
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-06-21 19:04:52 +0530

    Update dl_kdb.py

--- dl_kdb.py
+++ dl_kdb.py
@@ -106,50 +106,46 @@ if dl_type == "Activation Functions" :
 #-----------------------------------------------------------------------
     if a_f == 'Logistic (Sigmoid) Function':
 
-        #st.subheader('Logistic (Sigmoid) Function')
-        tab1, tab2, tab3, tab4 = st.tabs(["**Description**","**Plot**","**Derivative**","**Pros & Cons**"])
 
-        with tab1:
+        #st.subheader('Description')
+        st.write('It is a sigmoid function with a characteristic "S"-shaped curve.')
+        st.markdown(r'**$sigmoid(z)=\frac{1}{1+exp(-z)}$**')
+        st.write('The output of the logistic (sigmoid) function is always between 0 and 1.')   
 
-            #st.subheader('Description')
-            st.write('It is a sigmoid function with a characteristic "S"-shaped curve.')
-            st.markdown(r'**$sigmoid(z)=\frac{1}{1+exp(-z)}$**')
-            st.write('The output of the logistic (sigmoid) function is always between 0 and 1.')   
-
-        with tab2:
+        st.divider()
     
-            #st.subheader('Plot')
-            logistic_fig  = plot_function(logistic, title='Logistic (Sigmoid) Activation Function')
-            logistic_fig.add_annotation(x=7, y=1, text='<b>Saturation</b>', showarrow=True,font=dict(family="Montserrat", size=16, color="#1F8123"),
+        #st.subheader('Plot')
+        logistic_fig  = plot_function(logistic, title='Logistic (Sigmoid) Activation Function')
+        logistic_fig.add_annotation(x=7, y=1, text='<b>Saturation</b>', showarrow=True,font=dict(family="Montserrat", size=16, color="#1F8123"),
                                         align="center",arrowhead=2, arrowsize=1, arrowwidth=2, arrowcolor="#A835E1", ax=-20, ay=30,)
-            logistic_fig.add_annotation(x=-7, y=0, text='<b>Saturation</b>', showarrow=True,font=dict(family="Montserrat", size=16, color="#1F8123"),
+        logistic_fig.add_annotation(x=-7, y=0, text='<b>Saturation</b>', showarrow=True,font=dict(family="Montserrat", size=16, color="#1F8123"),
                                         align="center",arrowhead=2, arrowsize=1, arrowwidth=2, arrowcolor="#A835E1", ax=0, ay=-30,)
-            st.plotly_chart(logistic_fig)
+        st.plotly_chart(logistic_fig)
             
-            with st.expander('Plot Explanation'):
-                st.write('- The logistic function saturates as the inputs become larger (either positive or negative).')
-                st.write('- For large positive and negative values, the function gets asymptotically close to 1 and 0, respectively.')
-                st.write('- When the function saturates, its gradient becomes very close to zero, which slows down learning.')
+        with st.expander('Plot Explanation'):
+            st.write('- The logistic function saturates as the inputs become larger (either positive or negative).')
+            st.write('- For large positive and negative values, the function gets asymptotically close to 1 and 0, respectively.')
+            st.write('- When the function saturates, its gradient becomes very close to zero, which slows down learning.')
 
-        with tab3:
+        st.divider()
     
-            #st.subheader('Derivative')
-            st.markdown(r'$sigmoid^{\prime}(z)=sigmoid(z)(1−sigmoid(z))$')
-            st.text("")
-            logistic_der_fig = plot_function_derivative(logistic, title='Derivative of the Logistic Function')
-            st.plotly_chart(logistic_der_fig)
+        #st.subheader('Derivative')
+        st.markdown(r'$sigmoid^{\prime}(z)=sigmoid(z)(1−sigmoid(z))$')
+        st.text("")
+        logistic_der_fig = plot_function_derivative(logistic, title='Derivative of the Logistic Function')
+        st.plotly_chart(logistic_der_fig)
 
-            with st.expander('Plot Explanation'):
-                st.write('Notice that the derivative of the logistic function gets very close to zero for large positive and negative inputs.')
+        with st.expander('Plot Explanation'):
+            st.write('Notice that the derivative of the logistic function gets very close to zero for large positive and negative inputs.')
 
-        with tab4:
+        st.divider()
 
-            st.subheader('Pros')
-            st.write('1. The logistic function introduces non-linearity into the network which allows it to solve more complex problems than linear activation functions.\n2. It is continuous and differentiable everywhere.\n3. Because its output is between 0 and 1, it is very common to use in the output layer in binary classification problems.')
+        st.subheader('Pros')
+        st.write('1. The logistic function introduces non-linearity into the network which allows it to solve more complex problems than linear activation functions.\n2. It is continuous and differentiable everywhere.\n3. Because its output is between 0 and 1, it is very common to use in the output layer in binary classification problems.')
 
-            st.subheader('Cons')
-            st.write("1. Limited Sensitivity\n- The logistic function saturates across most of its domain.\n- It is only sensitive to inputs around its midpoint 0.5.")
-            st.write("2. Vanishing Gradients in Deep Neural Networks\n- Because the logistic function can get easily saturated with large inputs, its gradient gets very close to zero. This causes the gradients to get smaller and smaller as backpropagation progresses down to the lower layers of the network.\n- Eventually, the lower layers' weights receive very small updates and never converge to their optimal values.")
+        st.subheader('Cons')
+        st.write("1. Limited Sensitivity\n- The logistic function saturates across most of its domain.\n- It is only sensitive to inputs around its midpoint 0.5.")
+        st.write("2. Vanishing Gradients in Deep Neural Networks\n- Because the logistic function can get easily saturated with large inputs, its gradient gets very close to zero. This causes the gradients to get smaller and smaller as backpropagation progresses down to the lower layers of the network.\n- Eventually, the lower layers' weights receive very small updates and never converge to their optimal values.")
 
 
 
