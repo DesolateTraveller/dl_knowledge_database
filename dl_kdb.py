5fa09de3eb3a93e13f5f58c157fa1f9728e61a13
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-06-23 19:33:57 +0530

    Update dl_kdb.py

--- dl_kdb.py
+++ dl_kdb.py
@@ -285,13 +285,13 @@ if dl_type == "Activation Functions" :
         st.subheader("Randomized LeakyReLU (RReLU)",divider='blue')
 
         #st.title("Randomized LeakyReLU (RReLU)")
-        st.write('In this variant, the value of α is picked randomly in a given range during training, and is fixed to an average during testing.')
-        st.write('In addition to having the same advantages of the LeakyReLU, it also has a slight regularization effect (reduce overfitting).')
+        st.write("1. In this variant, the value of α is picked randomly in a given range during training, and is fixed to an average during testing.")
+        st.write("2. In addition to having the same advantages of the LeakyReLU, it also has a slight regularization effect (reduce overfitting).")
 
         st.divider()
-        
+
         st.subheader("Parametric LeakyReLU (PReLU)",divider='blue')
 
         #st.title('Parametric LeakyReLU (PReLU)')
-        st.write('In this variant, the value of α is a trainable (learnable) parameter rather than a hyperparameter.')
-        st.write('In other words, the backpropagation algorithm can tweak its value like any other model parameter.')
+        st.write("1. In this variant, the value of α is a trainable (learnable) parameter rather than a hyperparameter.")
+        st.write("2. In other words, the backpropagation algorithm can tweak its value like any other model parameter.")
